4218640bd172bb238bdf02bb2e7013bcab6bd617
pacifik13 <prashant.bazz@gmail.com>
2022-01-11 13:12:51 +0545

    results updated to last 7

--- components/Results.js
+++ components/Results.js
@@ -21,8 +21,9 @@ function Results() {
             })
     }, []);
 
-    var size = 7;
-    const showResult = result.slice(0, size)
+    // var size = 7;
+    var leng = result.length;
+    const showResult = result.slice(leng-7, leng)
 
     return (
         <div className='bg-pink-300 flex flex-1 items-center justify-start w-full h-40 m-2 overflow-x-scroll overflow-y-hidden scrollbar-hide'>
