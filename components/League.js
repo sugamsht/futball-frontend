a336edc0d387f6290cc3a663a9d1430bd0fd9b78
Sugam Shrestha <sugamsht143@gmail.com>
2022-01-12 23:31:37 +0545

    Fixed all warnings

--- components/League.js
+++ components/League.js
@@ -5,7 +5,7 @@ function League() {
         <div className='w-screen h-[40rem] m-2 bg-emerald-500'>
             <div className='grid grid-cols-2 gap-2 overflow-hidden h-full'>
                 <h1 className='col-span-2 text-blue-600 text-center font-extrabold text-5xl mt-6'>A Division League</h1>
-                <div col-span-1>
+                <div>
                     <p className='mx-5 pr-5 text-xs md:text-sm lg:text-xl'>Aenean urna risus, pellentesque id tincidunt in, euismod non lorem. Sed fringilla est vel malesuada euismod. Cras felis lectus, condimentum non dapibus eu, maximus ac justo. Vestibulum nisi massa,
                         vestibulum ut odio ut, scelerisque blandit libero. Nullam pretium commodo nisl eget ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam at consequat eros. In porttitor ante et justo malesuada bibendum.
                         Fusce cursus ante eu ante cursus rutrum. Curabitur bibendum dapibus ligula, a vestibulum sapien dapibus sit amet. Ut eget sollicitudin nibh. Sed vel maximus odio.
@@ -15,7 +15,7 @@ function League() {
                             alt="A division League Logo"></img>
                     </div>
                 </div>
-                <div className='col-span-1'>
+                <div>
                     <p className='mx-5 text-xs md:text-sm lg:text-xl'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis feugiat risus, eget tincidunt velit fermentum et. Etiam tellus nibh,
                         gravida ac lacinia ac, elementum a ex. Vivamus risus magna, volutpat lacinia erat ac, porta congue ipsum. Aenean rutrum id risus nec pulvinar. Sed augue ex, maximus tincidunt nisl eu,
                         faucibus scelerisque nulla. Duis bibendum vulputate ante at aliquam. Morbi at felis odio. Proin non lobortis nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras viverra ornare condimentum. Nunc elementum elit non ipsum sagittis, vitae tempus ex tempor.
--- components/PointsTable.js
+++ components/PointsTable.js
@@ -4,34 +4,34 @@ import React, { useState, useEffect } from 'react';
 
 function PointsTable() {
     const [stat, setStat] = useState([]);
-  
+
     useEffect(() => {
-      document.title = "Hawa Frontend"
-      axios.get('http://localhost:3000/api/teams')
-        .then(function (response) {
-          // handle success
-          const data = response.data;
-          console.log(data)
-          setStat(data)
-        })
-        .catch(function (error) {
-          // handle error
-          console.log(error);
-        })
+        document.title = "Hawa Frontend"
+        axios.get('http://localhost:3000/api/teams')
+            .then(function (response) {
+                // handle success
+                const data = response.data;
+                console.log(data)
+                setStat(data)
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
     }, []);
-  
+
     return (
         <LeagueTable stat={stat} />
     )
 }
 
 const LeagueTable = ({ stat }) => {
-        if (stat.length > 0) {
-          var i = 1;
-          var temp = "";
-          stat.forEach((itemData) => {
+    if (stat.length > 0) {
+        var i = 1;
+        var temp = "";
+        stat.forEach((itemData) => {
             //index of itemData.team
-      
+
             temp += "<tr>";
             temp += "<td class='mr-9 text-center'>" + i + "</td>";
             temp += "<td class='pl-3'>" + itemData.name + "</td>";
@@ -42,36 +42,36 @@ const LeagueTable = ({ stat }) => {
             temp += "<td class='text-center'>" + itemData.gd + "</td>";
             temp += "<td class='text-center'>" + itemData.points + "</td>";
             i++;
-          });
-          document.getElementById('data').innerHTML = temp;
-        }
+        });
+        document.getElementById('data').innerHTML = temp;
+    }
     return (
         <>
-        <div className='hidden md:block col-span-2 w-auto h-[30rem] mr-2 my-2 bg-gray-700 items-center justify-center float-right'>
-            <div class="text-white h-full bg-gray-800 overflow-scroll scrollbar-hide">
-                <h1 class="text-xs md:text-sm lg:text-1xl font-bold pt-2 px-1 text-center w-full">Nepal A Division Table</h1>
-                <div class="py-8">
-                    <div class="max-w-screen-xl px-2 mx-auto">
-                        <table class="w-full text-base">
-                            <thead>
-                                <tr class="border-b border-gray-600">
-                                    <th class="text-left p-1 pb-2">&nbsp;</th>
-                                    <th class="text-left p-1 pb-2 pl-3"><abbr title="Teams in Competition">TEAM</abbr></th>
-                                    <th class="text-left p-1 pb-2"><abbr title="Games Played">PLD</abbr></th>
-                                    <th class="text-left p-1 pb-2"><abbr title="Games Won">WON</abbr></th>
-                                    <th class="text-left p-1 pb-2"><abbr title="Games Drawn">DRN</abbr></th>
-                                    <th class="text-left p-1 pb-2"><abbr title="Games Lost">LST</abbr></th>
-                                    <th class="text-left p-1 pb-2"><abbr title="Goal Difference">GD</abbr></th>
-                                    <th class="text-left p-1 pb-2"><abbr title="Points">PTS</abbr></th>
-                                </tr>
-                            </thead>
-                            <tbody className='text-white h-full ' id="data">
-                            </tbody>
-                        </table>
+            <div className='hidden md:block col-span-2 w-auto h-[30rem] mr-2 my-2 bg-gray-700 items-center justify-center float-right'>
+                <div className="text-white h-full bg-gray-800 overflow-scroll scrollbar-hide">
+                    <h1 className="text-xs md:text-sm lg:text-1xl font-bold pt-2 px-1 text-center w-full">Nepal A Division Table</h1>
+                    <div className="py-8">
+                        <div className="max-w-screen-xl px-2 mx-auto">
+                            <table className="w-full text-base">
+                                <thead>
+                                    <tr className="border-b border-gray-600">
+                                        <th className="text-left p-1 pb-2">&nbsp;</th>
+                                        <th className="text-left p-1 pb-2 pl-3"><abbr title="Teams in Competition">TEAM</abbr></th>
+                                        <th className="text-left p-1 pb-2"><abbr title="Games Played">PLD</abbr></th>
+                                        <th className="text-left p-1 pb-2"><abbr title="Games Won">WON</abbr></th>
+                                        <th className="text-left p-1 pb-2"><abbr title="Games Drawn">DRN</abbr></th>
+                                        <th className="text-left p-1 pb-2"><abbr title="Games Lost">LST</abbr></th>
+                                        <th className="text-left p-1 pb-2"><abbr title="Goal Difference">GD</abbr></th>
+                                        <th className="text-left p-1 pb-2"><abbr title="Points">PTS</abbr></th>
+                                    </tr>
+                                </thead>
+                                <tbody className='text-white h-full ' id="data">
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
