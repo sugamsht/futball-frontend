0a89a9a40ddb8f4153abeb2999529d0b9df91fd5
Sugam Shrestha <sugamsht143@gmail.com>
2022-01-18 14:58:34 +0545

    story done

--- components/Story.js
+++ components/Story.js
@@ -26,7 +26,6 @@ function Story({ storyIndex }) {
     const story2 = [
         {
             content: ({ action, isPaused }) => {
-                action('play');
                 return (
                     <div>
                         <img style={image} src="https://i.ibb.co/MGbfDTH/Group-13.png" ></img>
@@ -36,13 +35,10 @@ function Story({ storyIndex }) {
         },
     ];
     const story3 = [
-
-
         {
             url: './video.mp4',
             type: 'video',
         },
-
     ];
 
     const story4 = [
@@ -66,9 +62,6 @@ function Story({ storyIndex }) {
         borderRadius: 4
     };
 
-    function startStory() {
-        setStory(!story);
-    }
 
     return (
         <div className='flex items-center justify-start h-full m-2'>
@@ -77,31 +70,39 @@ function Story({ storyIndex }) {
                     src="https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTgzODkzMjM5NzUxNzgwMDQx/sipa_35072412.jpg"
                     alt=""
                     className="absolute inset-0 w-full h-full object-cover object-bottom hover:opacity-75 transition duration-150 ease-in-out"
-                    onClick={startStory}
                     data-bs-toggle="modal" data-bs-target={'#centeredStory' + storyIndex}
+                    onClick={() => setStory(true)}
                 />
-                <div className="modal fade fixed top-6 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-                    onClick={() => setStory(false)}
-                    id={'centeredStory' + storyIndex} tabIndex="-1" aria-labelledby={'centeredStory' + storyIndex} aria-modal="true" role="dialog">
-                    <div className="modal-dialog relative w-auto pointer-events-none">
-                        <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto rounded-md outline-none text-current">
-                            <div className="modal-body relative p-4">
+                <div
+                    className="modal fade fixed top-6 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+                    id={'centeredStory' + storyIndex} tabIndex="-1" aria-labelledby={'centeredStory' + storyIndex} aria-modal="true" role="dialog"
+                    style={{
+                        background: "rgba(6, 2, 5, 0.4)",
+                        display: story ? 'block' : 'none'
+                    }}
+                >
+                    <div className="modal-dialog relative w-auto pointer-events-none" >
+                        {story && <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto rounded-md outline-none text-current"
+                        >
+                            <div className="modal-body relative p-4" >
                                 <button type="button"
-                                    className="btn-close box-content w-4 h-4 p-1 float-right text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                                    className="btn-close box-content w-4 h-4 p-1 float-right text-black border-none rounded-none opacity-90 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
                                     data-bs-dismiss="modal" aria-label="Close"
-                                    onClick={startStory}
+                                    onClick={() => setStory(false)}
                                 ></button>
                                 <div className='m-0'>
-                                    {story && <StoryComponent
+                                    <StoryComponent
                                         keyboardNavigation
                                         defaultInterval={3000}
                                         stories={array[storyIndex]}
                                         width={'100%'}
                                         height={'85vh'}
-                                    />}
+                                        onStoryEnd={() => setStory(false)}
+                                    />
                                 </div>
                             </div>
                         </div>
+                        }
                     </div>
                 </div>
             </div>
