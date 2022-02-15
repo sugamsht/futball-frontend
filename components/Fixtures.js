0154e91786963da44711777a1b4a9905722183e3
Sugam Shrestha <sugamsht143@gmail.com>
2022-02-15 09:53:48 +0545

    Toggle Between All Fixtures

--- components/Fixtures.js
+++ components/Fixtures.js
@@ -1,10 +1,44 @@
 import Fixture from './Fixture'
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Fixtures({ fixture }) {
+    // console.log(fixture);
 
-    var leng = fixture.length;
-    const showFixture = fixture.slice(leng - 7, leng)
+    fixture.sort(function (a, b) {
+        var dateA = new Date(a.date), dateB = new Date(b.date)
+        return dateA - dateB
+    });
+
+    const [showFixture, setShowFixture] = React.useState([]);
+
+    let leng = fixture.length;
+    useEffect(() => {
+        setShowFixture(fixture.slice(leng - 7, leng));
+    }, [fixture]);
+
+
+
+    if (typeof window !== "undefined") {
+
+        // browser code
+        window.addEventListener('load', function () {
+            var i = 0;
+            document.getElementById('prev_button').addEventListener('click', function (e) {
+                i <= leng - 14 && (i = i + 7);
+                console.log("Prev", i)
+                setShowFixture(fixture.slice(leng - (i + 7), leng - i));
+            }
+            );
+
+            document.getElementById('next_button').addEventListener('click', function (e) {
+                console.log("Next ", i)
+                i > 1 && (i = i - 7);
+                setShowFixture(fixture.slice(leng - (i + 7), leng - i));
+            }
+            );
+
+        });
+    }
 
     return (
         <div className='flex flex-col justify-center items-center my-2 w-full h-auto bg-green-500 overflow-y-scroll scrollbar-hide'>
@@ -15,6 +49,10 @@ function Fixtures({ fixture }) {
                     showFixture.map((fixture, index) => (
                         <Fixture team1={fixture.team1} key={index} team2={fixture.team2} time={fixture.time} date={fixture.date} />
                     ))}
+            </div>
+            <div>
+                <button className='p-2 mx-2 bg-white' id="prev_button">Previous</button>
+                <button className='p-2 mx-2 bg-white' id="next_button">Next!</button>
             </div>
         </div>
     )
--- components/LiveScore.js
+++ components/LiveScore.js
@@ -1,57 +1,63 @@
 import React from 'react'
+import Head from 'next/head'
 
 export default function LiveScore() {
     return (
-        <div class="sm:flex-wrap md:block col-span-7 lg:col-span-3 w-auto my-2 live-score-card">
-            <div class="w-full h-full flex flex-col bg-white">
-                <div class="flex border-b-2 border-gray-200 p-4">
-                    <hr />
-                    <div class="flex leading-4 bg-pink-100 text-red-700 mr-auto text-bold rounded py-2 px-3 match-status">
-                        • Live</div>
-                    <div class="flex text-bold match-tournament">
-                        <img class="mr-3 w-5" src="https://assets.codepen.io/285131/pl-logo.svg" />English Premier League</div>
-                    <div class="match-actions flex ml-auto">
-                        <button class="btn-icon"><i class="material-icons-outlined">grade</i></button>
-                        <button class="btn-icon"><i class="material-icons-outlined">add_alert</i></button>
-                    </div>
-                </div>
-                <div class="match-content flex relative">
-                    <div class="column">
-                        <div class="team team--home">
-                            <div class="team-logo">
-                                <img src="https://assets.codepen.io/285131/whufc.svg" />
-                            </div>
-                            <h2 class="team-name">West Ham</h2>
+        <>
+            <Head>
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined|Material+Icons" rel="stylesheet" />
+            </Head>
+            <div className="sm:flex-wrap md:block col-span-7 lg:col-span-3 w-auto my-2 live-score-card">
+                <div className="w-full h-full flex flex-col bg-white">
+                    <div className="flex border-b-2 border-gray-200 p-4">
+                        <hr />
+                        <div className="flex leading-4 bg-pink-100 text-red-700 mr-auto text-bold rounded py-2 px-3 match-status">
+                            • Live</div>
+                        <div className="flex text-bold match-tournament">
+                            <img className="mr-3 w-10" src="https://www.goalnepal.com/uploads/news/1546859794.png" />Martyr's Memorial A-Division</div>
+                        <div className="match-actions flex ml-auto">
+                            <button className="btn-icon"><i className="material-icons-outlined">grade</i></button>
+                            <button className="btn-icon"><i className="material-icons-outlined">add_alert</i></button>
                         </div>
                     </div>
-                    <div class="column">
-                        <div class="match-details">
-                            <div class="match-date">
-                                12 Aug at <strong>19:00</strong>
-                            </div>
-                            <div class="match-score">
-                                <span class="match-score-number match-score-number--leading">2</span>
-                                <span class="match-score-divider">:</span>
-                                <span class="match-score-number">0</span>
-                            </div>
-                            <div class="match-time-lapsed">
-                                72'
-                            </div>
-                            <div class="match-referee">
-                                Referee: <strong>Joseph Hicks</strong>
+                    <div className="match-content flex relative">
+                        <div className="column">
+                            <div className="team team--home">
+                                <div className="team-logo">
+                                    <img src="https://assets.codepen.io/285131/whufc.svg" />
+                                </div>
+                                <h2 className="team-name">West Ham</h2>
                             </div>
                         </div>
-                    </div>
-                    <div class="column">
-                        <div class="team team--away">
-                            <div class="team-logo">
-                                <img src="https://assets.codepen.io/285131/chelsea.svg" />
+                        <div className="column">
+                            <div className="match-details">
+                                <div className="match-date">
+                                    12 Aug at <strong>19:00</strong>
+                                </div>
+                                <div className="match-score">
+                                    <span className="match-score-number match-score-number--leading">2</span>
+                                    <span className="match-score-divider">:</span>
+                                    <span className="match-score-number">0</span>
+                                </div>
+                                <div className="match-time-lapsed">
+                                    72'
+                                </div>
+                                <div className="match-referee">
+                                    Referee: <strong>Joseph Hicks</strong>
+                                </div>
                             </div>
-                            <h2 class="team-name">Chelsea</h2>
+                        </div>
+                        <div className="column">
+                            <div className="team team--away">
+                                <div className="team-logo">
+                                    <img src="https://assets.codepen.io/285131/chelsea.svg" />
+                                </div>
+                                <h2 className="team-name">Chelsea</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
