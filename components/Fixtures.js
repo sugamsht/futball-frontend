572e645ee36cb3e59fbdb9d83bd89cb6308f2cbb
Sugam Shrestha <sugamsht143@gmail.com>
2022-03-07 16:53:47 +0545

    Fixed Fixtures View

--- components/Fixtures.js
+++ components/Fixtures.js
@@ -2,7 +2,6 @@ import Fixture from './Fixture'
 import React, { useEffect, useState } from 'react';
 
 function Fixtures({ fixture }) {
-    // console.log(fixture);
 
     fixture.sort(function (a, b) {
         var dateA = new Date(a.date), dateB = new Date(b.date)
@@ -23,35 +22,44 @@ function Fixtures({ fixture }) {
     const [theDay, setTheDay] = useState('');
 
     let leng = fixture.length;
-    // console.log("yo chai total", leng)
 
-    for (let i = 0; i < leng; i++) {
-        var datea = new Date(fixture[i].date);
-        if (datea >= today) {
-            // setShowFixture(fixture[i]);
-            // console.log('yeha dekhi leko', fixture[i]);
-            // console.log('yo index ho', i);
-            useEffect(() => {
-                setShowFixture(fixture.slice(i, i + 7));
-                setTheDay(i);
-            }, []);
-            break;
-        }
-        // else{
-        //     console.log('no match');
-        //     // break;
-        // }
+
+    //NoFixtures
+    const NoFixtures = () => {
+        return (
+            <div>
+                <h1 className='flex justify-center text-xl items-center mt-5'>No Fixtures today</h1>
+            </div>
+        )
     }
 
-
-
+    useEffect(() => {
+        for (let j = 0; j < leng; j++) {
+            var datea = new Date(fixture[j].date);
+            if (datea >= today) {
+                // setShowFixture(fixture[i]);
+                // console.log('yeha dekhi leko', fixture[i]);
+                console.log('yo index ho', j);
+                setShowFixture(fixture.slice(j, j + 7));
+                setTheDay(j);
+                break;
+            }
+            else {
+                // setShowFixture(fixture.slice(leng - 7, leng));
+                setTheDay(leng);
+            }
+        }
+    }, []);
 
     if (typeof window !== "undefined") {
 
         // browser code
         window.addEventListener('load', function () {
+
             // var i = 83;
             var i = theDay;
+            console.log("khoi ta daya", theDay)
+
             document.getElementById('prev_button').addEventListener('click', function (e) {
                 i >= 7 && (i = i - 7)
                 // i >= 7 ? (i = i - 7) : (i = 0); //this shows fixtures from start
@@ -62,24 +70,32 @@ function Fixtures({ fixture }) {
             );
 
             document.getElementById('next_button').addEventListener('click', function (e) {
-                console.log("Next ", i)
+
+
+                console.log("Next ", i);
                 i < leng - 7 && (i = i + 7);
                 setShowFixture(fixture.slice(i, i + 7));
+
+                // (showFixture.length == 0) ? console.log("yo ho fixturelength", showFixture.length) : console.log("Next ", i); i < leng - 7 && (i = i + 7);
+                // setShowFixture(fixture.slice(i, i + 7));
+
             }
             );
 
+
         });
     }
-
     return (
         <div className='flex flex-col justify-center items-center my-2 w-full h-auto bg-green-500 overflow-y-scroll scrollbar-hide'>
             <h1 className='text-blue-600 font-extrabold text-4xl mt-5'>Fixtures</h1>
-            <h2 className='text-gray-600 font-bold text-2xl mt-5'>Match Week 9</h2>
+            {/* <h2 className='text-gray-600 font-bold text-2xl mt-5'>Match Week 9</h2> */}
             <div className='sm:w-full md:w-full md:px-[10vw] lg:w-full 2xl:w-3/4 2xl:px-0 mb-10' >
                 {
-                    showFixture.map((fixture, index) => (
-                        <Fixture team1={fixture.team1} key={index} team2={fixture.team2} time={fixture.time} date={fixture.date} />
-                    ))}
+                    (showFixture.length == 0) ? NoFixtures() :
+                        showFixture.map((fixture, index) => (
+                            <Fixture team1={fixture.team1} key={index} team2={fixture.team2} time={fixture.time} date={fixture.date} />
+                        ))
+                }
             </div>
             <div>
                 <button className='p-2 mx-2 bg-white' id="prev_button">Previous</button>
@@ -87,6 +103,7 @@ function Fixtures({ fixture }) {
             </div>
         </div>
     )
+
 }
 
 export default Fixtures
